c9d775d6664024f151210c1e633080552390ff8f
wysiwyz <arriettybenjamin@gmail.com>
2024-07-18 12:02:15 +0800

    create react-app-local-state, import EIP-6963 and EIP-1193

---
+++ vite-react-local-state/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      <h2>Wallets Detected: </h2>
+    </div>
+  )
+}
+
+
+export default App
---
+++ vite-react-local-state/src/vite-env.d.ts
@@ -0,0 +1,50 @@
+/// <reference types="vite/client" />
+
+// Describes metadata related to a provider based on EIP-6963 (Wallet metadata announcement)
+interface EIP6963ProviderInfo {
+    walletId: string
+    uuid: string
+    name: string
+    icon: string
+}
+
+// Represents the structure of a provider based on EIP-1193 (Ethereum Provider JavaScript API)
+interface EIP1193Provider {
+    isStatus?: boolean
+    host?: string
+    path?: string
+    sendAsync?: (
+        request: { method: string; params?: Array<unknown> },
+        callback: (error: Error | null, response: unknown) => void
+    ) => void
+    send?: (
+        request: { method: string; params?: Array<unknown> },
+        callback: (error: Error | null, response: unknown) => void
+    ) => void
+    // 向節點或錢包發送請求
+    request: (request: {
+        method: string
+        params?: Array<unknown>
+    }) => Promise<unknown>
+}
+
+// Combines the provider's metadata with an actual provider object, creating a complete picture of a
+// wallet provider at a glance.
+interface EIP6963ProviderDetail {
+    info: EIP6963ProviderInfo
+    provider: EIP1193Provider
+}
+
+// Represents the structure of an event dispatched by a wallet to announce its presence based on EIP-6963.
+type EIP6963AnnounceProviderEvent = {
+    detail: {
+        info: EIP6963ProviderInfo
+        provider: EIP1193Provider
+    }
+}
+
+// An error object with optional properties, commonly encountered when handling eth_requestAccounts errors.
+interface MMError {
+    code?: string
+    message?: string
+}
